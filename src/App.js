22c7c2a3d971e4ea8407877783d4f5394bda00da
luhoro <luisashorowitz@gmail.com>
2023-06-28 10:30:58 -0300

    feat initial base react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './paginas/Home'
+import AboutMe from './paginas/AboutMe'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/aboutme' element={<AboutMe />} />
+        <Route path='*' element={<h1>Page not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
